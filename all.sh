cc025586934363ed924397b418161cf13050306b
Sam Gardner <stgardner4@tamu.edu>
2021-08-07 23:42:49 -0500

    generate product and productType metadata

--- all.sh
+++ all.sh
@@ -6,7 +6,11 @@ for productDir in "${productDirs[@]}"
 do
     cd $productDir
     bash generate.sh
-    cd $MY_DIR
+    cd $HOME/hdwx-operational/
     rsync -r $productDir/output/. /var/www/html/wx4stg/
 done
-
+mkdir -p $HOME/hdwx-operational/metadata/
+~/miniconda3/envs/HDWX/bin/python3 writeProductTypeData.py
+rsync -r $HOME/hdwx-operational/metadata/. /var/www/html/wx4stg/metadata
+rm -rf $HOME/hdwx-operational/metadata/
+cd $MY_DIR
