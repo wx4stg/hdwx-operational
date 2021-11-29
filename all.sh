3d70cc6ab418e8e79aea8b4f71b647c396f1dc1b
Sam Gardner <stgardner4@tamu.edu>
2021-11-28 23:56:20 -0600

    make products generate their own prod jsons

--- all.sh
+++ all.sh
@@ -8,10 +8,7 @@ do
     cd $productDir
     bash generate.sh
     cd $myDir
-    rsync -r $productDir/output/. /var/www/html/wx4stg/
+    outDir=`realpath $startingDir/$productDir/output/`/.
+    rsync -r $outDir /var/www/html/wx4stg/
 done
-mkdir -p $myDir/metadata/
-~/miniconda3/envs/HDWX/bin/python3 writeProductTypeData.py
-rsync -r $myDir/metadata/. /var/www/html/wx4stg/metadata
-rm -rf $myDir/metadata/
 cd $startingDir
