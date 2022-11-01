64f159c938c0d37a953a80f823a9f2bdfbc6ef41
Sam Gardner <stgardner4@tamu.edu>
2022-11-01 16:43:59 -0500

    add geocolor json template

--- HDWX_helpers.py
+++ HDWX_helpers.py
@@ -56,6 +56,14 @@ def writeJson(basePath, productID, runTime, fileName, validTime, gisInfo, reload
         dispFrames = 30
         productTypeID = 0
         totalFrameCount = -1
+    elif productID == 5:
+        productDesc = "GOES-16 CONUS GeoColor"
+        productPath = "products/satellite/goes16/geocolor/"
+        isFcst = False
+        fileExt = "png"
+        dispFrames = 48
+        productTypeID = 0
+        totalFrameCount = -1
     elif productID == 100:
         productDesc = "Mesonet Farm WxCenter"
         productPath = "products/mesonet/Farm/wxcenter/"
